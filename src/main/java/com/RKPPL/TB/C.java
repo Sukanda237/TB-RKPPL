3c371156d1fb83180d542a2e2b28302063ba9313
zamzamjamaludin <zamzam.jamaludin@mail.unpas.ac.id>
2016-05-23 16:09:08 +0700

    kelas C dan Math polos

---
+++ src/main/java/com/RKPPL/TB/C.java
@@ -0,0 +1,36 @@
+package com.RKPPL.TB;
+
+// Author : Zamzam Jamaludin A
+
+public class C {
+	double n1;
+	double n2;
+	
+	public C() {
+		System.out.println("Ctor di kelas C");
+	}
+	
+	public int add(){
+		
+		return 0;
+	}
+	
+	public int sub(){
+		
+		return 0;
+	}
+	
+	public double mul(){
+		return n1 * n2;
+	}
+	
+	public double div(){
+		return n1 / n2;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "toString di kelas C";
+	}
+}
---
+++ src/main/java/com/RKPPL/TB/Math.java
@@ -0,0 +1,14 @@
+package com.RKPPL.TB;
+
+//Author : Zamzam Jamaludin A
+
+public class Math extends C{
+
+	public int mod(int n1, int n2){
+		return n1 % n2;
+	}
+	
+	public double sqrt(double n){
+		return 3.14;
+	}
+}
