786138e51e884385d2ffc490c242f185cc8205b2
triasfz <triasfz@gmail.com>
2016-05-25 16:45:03 +0700

    Penambahan Kelas D

---
+++ src/main/java/com/RKPPL/TB/D.java
@@ -0,0 +1,15 @@
+package com.RKPPL.TB;
+
+//Author Trias Fachrul Zulfikar
+
+public class D {
+
+	public D(){
+		System.out.println("Ctor di kelas D");
+	}
+	
+	@Override
+	public String toString(){
+		return "toString di kelas D";
+	}
+}
