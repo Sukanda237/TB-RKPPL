26de37f0649b05550eee82490cfa12a7ff1f4df1
zamzamjamaludin <zamzam.jamaludin@mail.unpas.ac.id>
2016-05-27 17:15:38 +0700

    create MulAndDivInterface

---
+++ src/main/java/com/RKPPL/TB/MulAndDivInterface.java
@@ -0,0 +1,8 @@
+package com.RKPPL.TB;
+
+// author Zamzam Jamaludin Abdullah
+public interface MulAndDivInterface {
+	
+	public double mul(double n1, double n2);
+	public double div(double n1, double n2);
+}
