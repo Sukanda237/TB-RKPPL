9e9465cee4153c27288a2bc7ea0b609508189b9d
triasfz <triasfz@gmail.com>
2016-05-27 13:52:30 +0700

    pemanggilan kelas C dan D di MainApp, pemanggilan method dari setiap kelas

--- src/main/java/com/RKPPL/TB/MainApp.java
+++ src/main/java/com/RKPPL/TB/MainApp.java
@@ -7,7 +7,51 @@ public class MainApp {
 	
 	public static void main(String[] args) {
 		
+//		membuat variabel dan memasukan nilainya
+		int n1 = 5;
+		int n2 = 6;
 		
+//		Instansiasi kelas D
+		D d = new D();
+		
+//		method pow dari kelas D
+		System.out.println(d.pow(n1, n2));
+		
+//		method mul dari kelas D
+		System.out.println(d.mul(n1, n2));
+		
+//		method div dari kelas D
+		System.out.println(d.div(n1, n2));
+		
+//		method toString dari kelas D
+		System.out.println(d.toString());
+		
+		System.out.println();
+		
+		
+//		Instansiasi kelas C
+		C c = new C();
+		
+//		memasukan nilai ke n1 dan n2
+		c.setN1(n1);
+		c.setN2(n2);
+		
+//		method add dari kelas C
+		System.out.println(c.add());
+		
+//		method sub dari kelas C
+		System.out.println(c.sub());
+		
+//		method mul dari kelas C
+		System.out.println(c.mul());
+		
+//		method div dari kelas C
+		System.out.println(c.div());
+		
+//		method toString dari kelas C
+		System.out.println(c.toString());
+		
+		System.out.println();
 		
 	}
 
