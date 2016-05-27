0a0c7c43fc1f2b8836953ec0c537b90a52dd8a85
triasfz <triasfz@gmail.com>
2016-05-27 13:28:38 +0700

    Melakukan perbaikan terhadap kelas D, mengimplement dari kelas PowInterface

--- src/main/java/com/RKPPL/TB/D.java
+++ src/main/java/com/RKPPL/TB/D.java
@@ -2,10 +2,29 @@ package com.RKPPL.TB;
 
 //Author Trias Fachrul Zulfikar
 
-public class D {
+
+//Mengimplement dari kelas Pow Interface
+public class D implements PowInterface{
 
 	public D(){
 		System.out.println("Ctor di kelas D");
+	}
+	
+	@Override
+	public double pow(double n1, double n2){
+		n1 = 0.3;
+		n2 = 0.8;
+		return n1+n2;
+	}
+	
+	@Override
+	public double mul(double n1, double n2){
+		return n1*n2;
+	}
+	
+	@Override
+	public double div(double n1, double n2){
+		return n1/n2;
 	}
 	
 	@Override
