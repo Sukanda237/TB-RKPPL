ba93aee1991c563f7050d960230338f1536bc62e
triasfz <triasfz@gmail.com>
2016-05-27 14:26:39 +0700

    pembuatan method di dalam kelas E

--- src/main/java/com/RKPPL/TB/E.java
+++ src/main/java/com/RKPPL/TB/E.java
@@ -1,7 +1,34 @@
 package com.RKPPL.TB;
 
+import java.util.List;
+
 // Trias FZ
 
 public class E {
+	
+	private List dataList;
+	
+	public E(){
+		System.out.println("Ctor dari kelas E");
+	}
+	
+	public boolean containe(String n){
+		return true ;
+	}
 
+	public List getDataList() {
+		return dataList;
+	}
+
+	public void setDataList(List dataList) {
+		this.dataList = dataList;
+	}
+	
+	@Override
+	public String toString(){
+		return("toString di kelas E");
+	}
+	
+	
+		
 }
